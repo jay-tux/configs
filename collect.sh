d4133826ec7177d2223409f74f6638cfc668308c
Jay <sys.jonas@yahoo.com>
2020-02-10 11:58:25 +0100

    Updated config files

--- collect.sh
+++ collect.sh
@@ -2,8 +2,8 @@
 dir=`dirname "$0"`
 cp ~/.config/i3/config $dir/i3 2>/dev/null
 cp ~/.config/polybar/config $dir/polybar 2>/dev/null
-cp ~/.config/i3/ $dir/boot_polybar.sh 2>/dev/null
-cp ~/.config/i3/ $dir/boot_wallpaper.sh 2>/dev/null
+cp ~/.config/i3/boot_polybar.sh $dir/boot_polybar.sh 2>/dev/null
+cp ~/.config/i3/boot_wallpaper.sh $dir/boot_wallpaper.sh 2>/dev/null
 cp ~/.config/light $dir/light.sh 2>/dev/null
 cp ~/.config/light/pbar.sh $dir/light_polybar.sh 2>/dev/null
 cp ~/.config/ramswap/polybarmod.sh $dir/ram_swap.sh 2>/dev/null
